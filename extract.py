5fbf3245c8773e6a7f5f48fe2e7bef7ecf464d08
david <d.michaud@protonmail.com>
2026-02-15 15:49:58 +0100

    Add git ignore & fix a variable, add dependencies installation in readme

--- extract.py
+++ extract.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     with open(output_file, "w", encoding="utf-8") as f:
         f.write(text)
 
-    print("✔️ Extracted text saved to kumite_rules.txt")
+    print("✔️ Extracted text saved to " + output_file)
